6fa8505efb5fef1fcb31d7010a69420b45d5ff7d
Andreu Vall <andreu@pumas.ai>
2023-02-09 15:22:47 +0100

    Add more machine learning exercises

--- machine_learning.jl
+++ machine_learning.jl
@@ -34,13 +34,18 @@ function sample_regression(
     
     x = rand(Uniform(-1, 1), num_samples)
     ϵ = rand(Normal(0, stdev), num_samples)
+    y = true_function.(x) .+ ϵ
 
-    return x, true_function.(x) .+ ϵ
+    idx = sortperm(x)
+    x = x[idx]
+    y = y[idx]
+
+    return x, y
 end
 
 x, y = sample_regression(50; seed=1)
 f = scatter(x, y; color=:gray, label="data");
-ablines!(0, 1; label="intercept = 0, slope = 1 (true)")
+lines!(x, x; label="true")
 f
 
 xs = reshape(x, 1, :)
@@ -56,15 +61,160 @@ G = SimpleChains.alloc_threaded_grad(linreg)
 
 loss = SimpleChains.add_loss(linreg, SquaredLoss(ys))
 SimpleChains.train_unbatched!(G, p, loss, xs, SimpleChains.ADAM(), 10_000)
+y_hat = linreg(xs, p)
 
-fitted_slope, fitted_intercept = p[1], p[2]
-ablines!(
-  fitted_intercept, fitted_slope; 
-  label="intercept = $(@sprintf("%.2f", fitted_intercept)), slope = $(@sprintf("%.2f", fitted_slope)) (fitted)"
-)
+lines!(x, y_hat[:]; label="predicted")
 axislegend()
 f
 
+# EXAMPLE 2: MAKE true_function more COMPLEX
+
+x, y = sample_regression(50; true_function=x -> x^2, seed=1)
+f = scatter(x, y; color=:gray, label="data");
+lines!(x, x.^2)
+f
+
+xs = reshape(x, 1, :)
+ys = reshape(y, 1, :)
+
+linreg = SimpleChain(
+  static(1),
+  TurboDense{true}(identity, 1)
+)
+
+p = SimpleChains.init_params(linreg)
+G = SimpleChains.alloc_threaded_grad(linreg)
+
+loss = SimpleChains.add_loss(linreg, SquaredLoss(ys))
+SimpleChains.train_unbatched!(G, p, loss, xs, SimpleChains.ADAM(), 10_000)
+y_hat = linreg(xs, p)
+
+lines!(x, y_hat[:]; label="predicted")
+axislegend()
+f
+
+mlp = SimpleChain(
+  static(1),
+  TurboDense{true}(tanh, 8),
+  TurboDense{true}(identity, 1)
+)
+
+p = SimpleChains.init_params(mlp)
+G = SimpleChains.alloc_threaded_grad(mlp)
+
+loss = SimpleChains.add_loss(mlp, SquaredLoss(ys))
+SimpleChains.train_unbatched!(G, p, loss, xs, SimpleChains.ADAM(), 10_000)
+
+y_hat = mlp(xs, p)
+
+f = scatter(x, y; color=:gray, label="data");
+lines!(x, true_function.(x))
+lines!(x, y_hat[:]; label="predicted")
+axislegend()
+f
+
+# EXAMPLE 3: OVERFITTING
+
+# REPEAT SAME BUT Train much longer with same architecture for overfitting
+
+mlp = SimpleChain(
+  static(1),
+  TurboDense{true}(tanh, 8),
+  TurboDense{true}(identity, 1)
+)
+
+p = SimpleChains.init_params(mlp)
+G = SimpleChains.alloc_threaded_grad(mlp)
+
+loss = SimpleChains.add_loss(mlp, SquaredLoss(ys))
+SimpleChains.train_unbatched!(G, p, loss, xs, SimpleChains.ADAM(), 10_000_000)
+
+y_hat = mlp(xs, p)
+
+f = scatter(x, y; color=:gray, label="data");
+lines!(x, true_function.(x))
+lines!(x, y_hat[:]; label="predicted")
+axislegend()
+f
+
+# EXAMPLE 4: OVERFITTING BY MODEL CAPACITY AND TOO LONG TRAINING
+
+mlp = SimpleChain(
+  static(1),
+  TurboDense{true}(tanh, 128),
+  TurboDense{true}(tanh, 128),
+  TurboDense{true}(identity, 1)
+)
+
+p = SimpleChains.init_params(mlp)
+G = SimpleChains.alloc_threaded_grad(mlp)
+
+loss = SimpleChains.add_loss(mlp, SquaredLoss(ys))
+SimpleChains.train_unbatched!(G, p, loss, xs, SimpleChains.ADAM(), 1_000_000)
+
+y_hat = mlp(xs, p)
+
+f = scatter(x, y; color=:gray, label="data");
+lines!(x, true_function.(x))
+lines!(x, y_hat[:]; label="predicted")
+axislegend()
+f
+
+
+# EXERCISE 5: CONTROLING VALIDATION LOSS
+
+x_train, y_train = sample_regression(50; true_function=x -> x^2)
+x_valid, y_valid = sample_regression(50; true_function=x -> x^2)
+
+f = scatter(x_train, y_train; color=:gray, label="training");
+scatter!(x_valid, y_valid; label="validation");
+axislegend()
+f
+
+mlp = SimpleChain(
+  static(1),
+  TurboDense{true}(tanh, 128),
+  TurboDense{true}(tanh, 128),
+  TurboDense{true}(identity, 1)
+)
+
+xs_train, ys_train = reshape(x_train, 1, :), reshape(y_train, 1, :)
+xs_valid, ys_valid = reshape(x_valid, 1, :), reshape(y_valid, 1, :)
+
+p = SimpleChains.init_params(mlp)
+G = SimpleChains.alloc_threaded_grad(mlp)
+
+loss_train = SimpleChains.add_loss(mlp, SquaredLoss(ys_train))
+loss_valid = SimpleChains.add_loss(mlp, SquaredLoss(ys_valid))
+
+loss_train_l, loss_valid_l = [], []
+epoch_range = 1:200
+for epoch in epoch_range
+  SimpleChains.train_unbatched!(G, p, loss_train, xs_train, SimpleChains.ADAM(), 100);
+  push!(loss_train_l, loss_train(xs_train, p))
+  push!(loss_valid_l, loss_valid(xs_valid, p))
+end
+
+f, ax = scatterlines(
+  epoch_range, Float32.(loss_train_l); 
+  label="training",
+  axis = (; xlabel = "~Epochs", ylabel="Loss")
+)
+scatterlines!(epoch_range, Float32.(loss_valid_l); label="validation")
+axislegend()
+f
+
+f, ax = scatterlines(
+  epoch_range[1:20], Float32.(loss_train_l)[1:20]; 
+  label="training",
+  axis = (; xlabel = "~Epochs", ylabel="Loss")
+)
+scatterlines!(epoch_range[1:20], Float32.(loss_valid_l)[1:20]; label="validation")
+axislegend()
+f
+
+
+# EXERCISE 6: CLASSIFICATION EXAMPLE
 
 # can not have a simple classification example because SimpleChains doesn't have BCE yet
 
