a6dab44ed8c5e4763134d319d90d4ae9e000940e
Andreu Vall <andreu@pumas.ai>
2023-02-11 21:31:43 +0000

    Work on examples for augmentation of parameters

--- 03_augmenting_parameters.jl
+++ 03_augmenting_parameters.jl
@@ -1,5 +1,4 @@
 using DeepPumas
-using Pumas
 using StableRNGs
 using PumasUtilities
 using CairoMakie
@@ -7,25 +6,97 @@ using CairoMakie
 """
     true_function(x)
 
-Starting from x=65, increase slowly up until 1.
+Starting from x=65, increase slowly from 1 up until 2.
 
 # Examples
 ```jldoctest
 julia> true_function.([55, 65, 75, 85, 95])
 5-element Vector{Real}:
- 0
- 0
- 0.6321205588285577
- 0.8646647167633873
- 0.950212931632136
+ 1
+ 1
+ 1.6321205588285577
+ 1.8646647167633872
+ 1.9502129316321362
 ```
 """
 function true_function(x)
-    x <= 65 ? 0 : 1 - exp((65 - x) / 10)
+    x <= 65 ? 1 : 2 - exp((65 - x) / 10)
 end
 
-""" Data generating model with covariate `Age` affecting `Cl`earance. """
-data_model = @model begin
+"""
+Plot scatterplots for all input-ouput pairs in `target`.
+"""
+function plot(target::DeepPumas.FitTarget)
+    fig = Figure()
+    for i in 1:numinputs(target)
+        for j in 1:numoutputs(target)
+            scatter(
+                fig[i, j],target.x[i, :], target.y[j, :], 
+                axis = (
+                    xlabel = "covariate[$j]",
+                    ylabel = "η[$i]", 
+                )
+            )
+        end
+    end
+    fig
+end
+
+"""
+Plot all raw-to-raw scatterplots between matrices `A` and `B`.
+"""
+function pair_plots(A::Matrix, B::Matrix)
+
+    num_plot_rows, num_plot_cols = size(A)[1], size(B)[1]
+
+    fig = Figure()
+    for i in 1:num_plot_rows
+        for j in 1:num_plot_cols
+            scatter(fig[i, j], A[i, :], B[j, :])
+        end
+    end
+    fig
+end
+
+""" 
+Pumas model generating data where the deviation of each subject from 
+`tvCl` and `tvVc` is deterministically determined by `age` and `weight`.
+"""
+model_deterministic = @model begin
+    @param begin
+        tvCl ∈ RealDomain(lower = 0)        # typical value of clearance (L/hr)
+        tvVc ∈ RealDomain(lower = 0)        # typical value of central volume of distribution (L)
+        Ω ∈ PDiagDomain(2)                  # covariance matrix of random effects (between subject variability)
+        σ ∈ RealDomain(lower = 0)           # residual error
+    end
+    @covariates age weight
+    @pre begin
+        Cl = tvCl * true_function(age)      # per subject clearance (L/hr)
+        Vc = tvVc * true_function(weight)   # per subject volume of central compartment (L)
+    end
+    @dynamics begin
+        Central' = -(Cl / Vc) * Central     # ODE for concentration of drug in plasma (μg/L)
+    end
+    @derived begin
+        cp := @. 1000 * (Central / Vc)      # x1000 to match dose (mg) and concentration (μg/L)
+        dv ~ @. Normal(cp, σ)
+    end
+end
+
+population_deterministic = synthetic_data(
+    model_deterministic;
+    covariates = (
+        age = truncated(Normal(55, 10), 35, Inf),
+        weight = truncated(Normal(75, 10), 60, Inf),
+    ),
+    rng = StableRNG(0),
+)
+
+""" 
+Pumas model generating data where the deviation of each subject from 
+`tvCl` and `tvVc` is independent of `age` and `weight`.
+"""
+model_independent = @model begin
     @param begin
         tvCl ∈ RealDomain(lower = 0)    # typical value of clearance (L/hr)
         tvVc ∈ RealDomain(lower = 0)    # typical value of central volume of distribution (L)
@@ -35,9 +106,9 @@ data_model = @model begin
     @random begin
         η ~ MvNormal(Ω)                 # per subject random effects
     end
-    @covariates Age
+    @covariates age weight
     @pre begin
-        Cl = tvCl * exp(η[1]) + true_function(Age)  # per subject clearance (L/hr)
+        Cl = tvCl * exp(η[1])           # per subject clearance (L/hr)
         Vc = tvVc * exp(η[2])           # per subject volume of central compartment (L)
     end
     @dynamics begin
@@ -49,50 +120,49 @@ data_model = @model begin
     end
 end
 
-population = synthetic_data(
-    data_model;
-    covariates = (Age = truncated(Normal(65, 10), 35, Inf),),
+population_independent = synthetic_data(
+    model_independent;
+    covariates = (
+        age = truncated(Normal(55, 10), 35, Inf),
+        weight = truncated(Normal(75, 10), 60, Inf),
+    ),
     rng = StableRNG(0),
 )
 
-preds = predict(data_model, population[1:4], init_params(data_model))
-plotgrid(preds)
-
-""" Model with a universal differential equation. """
-ude_model = @model begin
-    @param begin
-        mlp ∈ MLP(3, 4, 4, (1, identity); reg = L2(1.0))
-        tvCl ∈ RealDomain(lower = 0)
-        tvVc ∈ RealDomain(lower = 0)
-        Ω ∈ PDiagDomain(2)
-        σ ∈ RealDomain(lower = 0)
-    end
-    @random begin
-        η ~ MvNormal(Ω)
-    end
-    @pre begin
-        Cl = tvCl * exp(η[1])
-        Vc = tvVc * exp(η[2])
-        mlp_ = only ∘ mlp
-    end
-    @dynamics begin
-        Central' = mlp_(Cl, Vc, Central)
-    end
-    @derived begin
-        cp := @. 1000 * (Central / Vc)
-        dv ~ @. Normal(cp, σ)
-    end
-end
-
-fitted_ude_model = fit(
-    ude_model,
-    population,
-    init_params(ude_model),
-    MAP(FOCE());
-    diffeq_options = (alg = Rodas5P(),),
-    optim_options = (iterations = 150,),
+target_model_independent_population_deterministic = preprocess(
+    model_independent, 
+    population_deterministic, 
+    init_params(model_independent), 
+    FOCE()
 )
 
-pred = predict(ude_model, population[1:4], coef(fitted_ude_model); obstimes = 0:0.1:10);
-pred = predict(ude_model, population[1:4], coef(fitted_ude_model); obstimes = 0:0.1:10);
-plotgrid(pred[1:4])
+target_model_independent_population_independent = preprocess(
+    model_independent, 
+    population_independent, 
+    init_params(model_independent), 
+    FOCE()
+)
+
+pair_plots(
+    target_model_independent_population_deterministic.x, 
+    target_model_independent_population_deterministic.y
+)
+pair_plots(
+    target_model_independent_population_independent.x,
+    target_model_independent_population_independent.y
+)
+
+mlp_domain = MLP(
+    numinputs(target_model_independent_population_deterministic), 
+    16, 16, 
+    (numoutputs(target_model_independent_population_deterministic), identity), 
+    reg = L2()
+)
+
+ho = hyperopt(mlp_domain, target_model_independent_population_deterministic)
+
+mlp_model = ho.ml.ml.model
+mlp_parameters = ho.ml.ml.param
+y_hat = mlp_model(target_model_independent_population_deterministic.x, mlp_parameters)
+
+pair_plots(target_model_independent_population_deterministic.x, Array(y_hat))
